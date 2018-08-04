e0faf44d94fabba55714b0047dd7f7e332f226b2
aaazureee <aaazureee@gmail.com>
2018-08-04 20:14:56 +1000

    Invite member component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,14 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
-import logo from './logo.svg';
 import './App.css';
 
 import Sidebar from './component/Sidebar';
+import InviteMember from './component/InviteMember';
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/*<div className="App">*/}
-        {/*<Sidebar />*/}
-        {/*<header className="App-header">*/}
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        {/*<h1 className="App-title">Welcome to React</h1>*/}
-        {/*</header>*/}
-        {/*<p className="App-intro">*/}
-        {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
-        {/*</p>*/}
-        {/*<Button>Hello wolrld</Button>*/}
-        {/*</div>*/}
-        <Sidebar/>
+        <InviteMember />
       </div>
     );
   }
