7423a3b970588fb1937f2f8ae630515e5ffd78ab
Amakawa <firesidecake@gmail.com>
2018-08-04 14:34:54 +1000

    add component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
 import logo from './logo.svg';
 import './App.css';
+
+import Sidebar from './component/Sidebar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {/*<div className="App">*/}
+        {/*<Sidebar />*/}
+        {/*<header className="App-header">*/}
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        {/*<h1 className="App-title">Welcome to React</h1>*/}
+        {/*</header>*/}
+        {/*<p className="App-intro">*/}
+        {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
+        {/*</p>*/}
+        {/*<Button>Hello wolrld</Button>*/}
+        {/*</div>*/}
+        <Sidebar/>
       </div>
     );
   }
